bcf89c4bf373d69f421d18d8f79a3e2174beafa3
Kirill Ivanov <bragov4ik@hotmail.com>
2023-03-28 01:56:14 +0300

    Better graph traits

--- src/common.rs
+++ src/common.rs
@@ -1,10 +1,90 @@
 pub trait Graph {
     type NodeIdentifier;
-    type NodeIdentifiers: Iterator<Item = Self::NodeIdentifier>;
+    type NodeIdentifiers;
 
-    fn neighbors(&self, node: &Self::NodeIdentifier) -> Self::NodeIdentifiers;
+    fn neighbors(&self, node: &Self::NodeIdentifier) -> Option<Self::NodeIdentifiers>;
 }
 
-pub trait GraphWithStart: Graph {
-    fn starting_nodes(&self) -> Self::NodeIdentifiers;
+impl<G: Graph> Graph for &G {
+    type NodeIdentifier = G::NodeIdentifier;
+    type NodeIdentifiers = G::NodeIdentifiers;
+
+    fn neighbors(&self, node: &Self::NodeIdentifier) -> Option<Self::NodeIdentifiers> {
+        (*self).neighbors(node)
+    }
+}
+
+pub trait Directed: Graph {
+    fn in_neighbors(&self, node: &Self::NodeIdentifier) -> Option<Self::NodeIdentifiers>;
+    fn out_neighbors(&self, node: &Self::NodeIdentifier) -> Option<Self::NodeIdentifiers>;
+}
+
+impl<G: Directed> Directed for &G {
+    fn in_neighbors(&self, node: &Self::NodeIdentifier) -> Option<Self::NodeIdentifiers> {
+        (*self).in_neighbors(node)
+    }
+
+    fn out_neighbors(&self, node: &Self::NodeIdentifier) -> Option<Self::NodeIdentifiers> {
+        (*self).out_neighbors(node)
+    }
+}
+
+pub trait Reversable<'a, TReverse = ReversedGraph<'a, Self>>: Directed {
+    fn reverse(&'a self) -> TReverse;
+}
+
+impl<'a, G> Reversable<'a, ReversedGraph<'a, G>> for G
+where
+    G: Directed,
+{
+    fn reverse(&'a self) -> ReversedGraph<'a, G> {
+        self.into()
+    }
+}
+
+pub struct ReversedGraph<'a, G> {
+    inner: &'a G,
+}
+
+impl<'a, G> From<&'a G> for ReversedGraph<'a, G> {
+    fn from(value: &'a G) -> Self {
+        ReversedGraph { inner: value }
+    }
+}
+
+impl<'a, G> Graph for ReversedGraph<'a, G>
+where
+    G: Graph,
+{
+    type NodeIdentifier = G::NodeIdentifier;
+    type NodeIdentifiers = G::NodeIdentifiers;
+
+    fn neighbors(&self, node: &Self::NodeIdentifier) -> Option<Self::NodeIdentifiers> {
+        self.inner.neighbors(node)
+    }
+}
+
+impl<'a, G> Directed for ReversedGraph<'a, G>
+where
+    G: Directed,
+{
+    fn in_neighbors(&self, node: &Self::NodeIdentifier) -> Option<Self::NodeIdentifiers> {
+        self.inner.out_neighbors(node)
+    }
+
+    fn out_neighbors(&self, node: &Self::NodeIdentifier) -> Option<Self::NodeIdentifiers> {
+        self.inner.in_neighbors(node)
+    }
+}
+
+impl<'a, G> Reversable<'a, &'a G> for ReversedGraph<'a, G>
+where
+    G: Directed,
+{
+    fn reverse(&'a self) -> &G
+    where
+        Self: Sized,
+    {
+        self.inner
+    }
 }
